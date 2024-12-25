5cfcc6a9e9b1dce5fda04c76f91671d477556133
urmilarawat005 <u4urmilarawat@gmail.com>
2024-12-25 14:19:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 function highlight() {
     //Write your code here
+	var boldWords = document.querySelectorAll("strong");
+
+	boldWords.forEach(function(word){
+		word.style.color = 'green';
+	});
 
 
 }
 
 
 function return_normal() {
+
+	var boldWords = document.querySelectorAll("strong");
+
+	boldWords.forEach(function(word){
+		word.style.color = 'black'
+	})
     //Write your code here
 
     
